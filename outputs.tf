e217c903662191ae4cd2425aa3bc445bc5428f07
He Guimin <guimin.hgm@alibaba-inc.com>
2017-09-12 15:51:42 +0800

    add new modules

---
+++ outputs.tf
@@ -0,0 +1,8 @@
+// Output the ID of the new VPC and Security Group created
+output "vpc_id" {
+  value = "${var.vpc_id}"
+}
+
+output "security_group_id" {
+  value = "${var.group_id == "" ? alicloud_security_group.group.id : var.group_id}"
+}
