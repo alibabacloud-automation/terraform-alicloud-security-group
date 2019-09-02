92c1638b9a306e94d3ec0e38d9ab2acebe54e949
ASaiun <272968632@qq.com>
2019-09-02 19:25:16 +0800

    Update main.tf
    remove the count for the alicloud_security_group resouce

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 // Security Group Resource for Module
 resource "alicloud_security_group" "group" {
-  count       = "${var.group_id == "" ? 1 : 0}"
   name        = "${var.group_name}"
   vpc_id      = "${var.vpc_id}"
   description = "${var.group_description}"
@@ -16,7 +15,7 @@ resource "alicloud_security_group_rule" "rules_cidr" {
   policy            = "${element(var.policies, count.index)}"
   port_range        = "${element(var.port_ranges, count.index)}"
   priority          = "${element(var.priorities, count.index)}"
-  security_group_id = "${var.group_id == "" ? alicloud_security_group.group[count.index].id : var.group_id}"
+  security_group_id = "${var.group_id == "" ? alicloud_security_group.group.id : var.group_id}"
   cidr_ip           = "${element(var.cidr_ips, count.index)}"
 }
 
@@ -29,7 +28,7 @@ resource "alicloud_security_group_rule" "rules_group" {
   policy                     = "${element(var.policies, count.index)}"
   port_range                 = "${element(var.port_ranges, count.index)}"
   priority                   = "${element(var.priorities, count.index)}"
-  security_group_id          = "${var.group_id == "" ? alicloud_security_group.group[count.index].id : var.group_id}"
+  security_group_id          = "${var.group_id == "" ? alicloud_security_group.group.id : var.group_id}"
   source_security_group_id   = "${element(var.source_security_group_ids, count.index)}"
   source_group_owner_account = "${element(var.source_group_owner_accounts, count.index)}"
 }
