18cc94d45f8ac09f52fb3f213c64f078af2f0107
He Guimin <guimin.hgm@alibaba-inc.com>
2019-12-19 11:54:18 +0800

    improve(ingress): support list cidr_blocks

--- locals.tf
+++ locals.tf
@@ -1,6 +1,9 @@
-// If there is not specifying vpc_id, the module will launch a new vpc
-resource "alicloud_vpc" "vpc" {
-  count      = var.create_vpc ? 1 : 0
-  cidr_block = var.vpc_cidr
-  name       = var.vpc_name == "" ? var.this_module_name : var.vpc_name
+locals {
+  default_name = "terraform-sg-${random_uuid.this.result}"
+  group_id     = var.existing_group_id != "" ? var.existing_group_id : var.create ? concat(alicloud_security_group.this.*.id, [""])[0] : ""
+  group_name   = var.group_name != "" ? var.group_name : var.this_module_name != "" ? var.this_module_name : var.name != "" ? var.name : local.default_name
+  description  = var.group_description != "" ? var.group_description : var.description
+
 }
+
+resource "random_uuid" "this" {}
--- outputs.tf
+++ outputs.tf
@@ -2,20 +2,20 @@
 
 output "this_security_group_id" {
   description = "The ID of the security group"
-  value       = var.group_id == "" ? alicloud_security_group.this.0.id : var.group_id
+  value       = local.group_id
 }
 
 output "this_security_group_vpc_id" {
   description = "The VPC ID"
-  value       = var.vpc_id == "" ? alicloud_vpc.vpc.0.id : var.vpc_id
+  value       = var.vpc_id
 }
 
 output "this_security_group_name" {
   description = "The name of the security group"
-  value       = alicloud_security_group.this.0.name
+  value       = concat(alicloud_security_group.this.*.name, [""])[0]
 }
 
 output "this_security_group_description" {
   description = "The description of the security group"
-  value       = alicloud_security_group.this.0.description
+  value       = concat(alicloud_security_group.this.*.description, [""])[0]
 }
