14de831a6b3c8c5d3a2ab1494dfe38d745f49a75
He Guimin <guimin.hgm@alibaba-inc.com>
2020-02-02 12:01:27 +0800

    update modules using the latest parameters

--- modules/cassandra/variables.tf
+++ modules/cassandra/variables.tf
@@ -4,20 +4,24 @@
 
 variable "region" {
   description = "The region used to launch this module resources."
+  type        = string
   default     = ""
 }
 
 variable "profile" {
   description = "The profile name as set in the shared credentials file. If not set, it will be sourced from the ALICLOUD_PROFILE environment variable."
+  type        = string
   default     = ""
 }
 variable "shared_credentials_file" {
   description = "This is the path to the shared credentials file. If this is not set and a profile is specified, $HOME/.aliyun/config.json will be used."
+  type        = string
   default     = ""
 }
 
 variable "skip_region_validation" {
   description = "Skip static validation of region ID. Used by users of alternative AlibabaCloud-like APIs or users w/ access to regions that are not public (yet)."
+  type        = bool
   default     = false
 }
 
@@ -26,7 +30,7 @@ variable "skip_region_validation" {
 #################
 
 variable "create" {
-  description = "Whether to create security group, If set `false` you must set `existing_group_id`."
+  description = "Whether to create security group. If false, you can specify an existing security group by setting `existing_group_id`."
   type        = bool
   default     = true
 }
@@ -50,7 +54,8 @@ variable "description" {
 }
 
 variable "existing_group_id" {
-  description = "ID of existing security group. If set, you should specify `create` as false to avoid creating redundant security group. (If this field has been set, you can set `create` to false at any time.)"
+  description = "ID of existing security group. If set, the `create` will be ignored."
+  type        = string
   default     = ""
 }
 
@@ -70,43 +75,38 @@ variable "ingress_rules" {
   default     = []
 }
 
-variable "ingress_with_cidr_block" {
-  description = "List of ingress rules to create where 'ingress_cidr_block' is not used. Each element's `cidr_block` can not be empty."
-  type        = list(map(string))
-  default     = []
-}
-variable "ingress_with_cidr_blocks" {
-  description = "List of ingress rules to create where 'ingress_cidr_block' is used. Each element's `cidr_block` will be ignored."
-  type        = list(map(string))
-  default     = []
-}
-variable "ingress_with_source_security_group_id" {
-  description = "List of ingress rules to create where 'source_security_group_id' is used"
-  type        = list(map(string))
-  default     = []
-}
-
-variable "ingress_with_ports" {
-  description = "The port list to use on all ingress ports rules, from port and to port is same in this way."
-  type        = list(number)
-  default     = []
-}
-variable "protocol_for_ingress_with_ports" {
-  description = "A protocol is used when setting `ingress_with_ports`."
-  type        = string
-  default     = "tcp"
-}
-
-variable "ingress_cidr_blocks" {
-  description = "The IPv4 CIDR ranges list to use on ingress cidrs rules. It's length up to 20 and more items will be ignored."
-  type        = list(string)
-  default     = []
-}
-
 variable "priority_for_ingress_rules" {
   description = "A priority is used when setting `ingress_rules`. Default to `default_ingress_priority`."
   type        = number
   default     = 1
+}
+
+variable "ingress_with_cidr_blocks" {
+  description = "List of ingress rules to create where 'cidr_blocks' is used. The valid keys contains `cidr_blocks`, `from_port`, `to_port`, `protocol`, `description`, `priority` and `rule`."
+  type        = list(map(string))
+  default     = []
+}
+variable "ingress_with_source_security_group_id" {
+  description = "List of ingress rules to create where `source_security_group_id` is used"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "ingress_with_cidr_blocks_and_ports" {
+  description = "List of ingress rules to create where `cidr_blocks` and `ports` is used. The valid keys contains `cidr_blocks`, `ports`, `protocol`, `description` and `priority`. The ports item's `from` and `to` have the same port. Example: '80,443' means 80/80 and 443/443."
+  type        = list(map(string))
+  default     = []
+}
+variable "ingress_ports" {
+  description = "The port list used on `ingress_with_cidr_blocks_and_ports` ports rules."
+  type        = list(number)
+  default     = []
+}
+
+variable "ingress_cidr_blocks" {
+  description = "The IPv4 CIDR ranges list to use on ingress cidrs rules."
+  type        = list(string)
+  default     = []
 }
 
 variable "default_ingress_priority" {
@@ -123,15 +123,14 @@ variable "egress_rules" {
   type        = list(string)
   default     = []
 }
-
-variable "egress_with_cidr_block" {
-  description = "List of egress rules to create where 'cidr_blocks' is used"
-  type        = list(map(string))
-  default     = []
+variable "priority_for_egress_rules" {
+  description = "A priority where `egress_rules` is used. Default to `default_egress_priority`."
+  type        = number
+  default     = 1
 }
 
 variable "egress_with_cidr_blocks" {
-  description = "List of egress rules to create where 'egress_cidr_block' is used. Each element's `cidr_block` will be ignored."
+  description = "List of egress rules to create where 'cidr_blocks' is used. The valid keys contains `cidr_blocks`, `from_port`, `to_port`, `protocol`, `description` and `priority`."
   type        = list(map(string))
   default     = []
 }
@@ -142,28 +141,21 @@ variable "egress_with_source_security_group_id" {
   default     = []
 }
 
-variable "egress_with_ports" {
-  description = "The port list to use on all egress ports rules, from port and to port is same in this way."
+variable "egress_with_cidr_blocks_and_ports" {
+  description = "List of egress rules to create where `cidr_blocks` and `ports` is used. The valid keys contains `cidr_blocks`, `ports`, `protocol`, `description` and `priority`. The ports item's `from` and `to` have the same port. Example: '80,443' means 80/80 and 443/443."
+  type        = list(map(string))
+  default     = []
+}
+variable "egress_ports" {
+  description = "The port list used on `egress_with_cidr_blocks_and_ports` ports rules."
   type        = list(number)
   default     = []
 }
 
-variable "protocol_for_egress_with_ports" {
-  description = "A protocol is used when setting `egress_with_ports`."
-  type        = string
-  default     = "tcp"
-}
-
 variable "egress_cidr_blocks" {
-  description = "The IPv4 CIDR ranges list to use on egress cidrs rules. It's length up to 20 and more items will be ignored."
+  description = "The IPv4 CIDR ranges list to use on egress cidrs rules."
   type        = list(string)
   default     = []
-}
-
-variable "priority_for_egress_rules" {
-  description = "A priority is used when setting `egress_rules`. Default to `default_egress_priority`."
-  type        = number
-  default     = 1
 }
 
 variable "default_egress_priority" {
@@ -174,42 +166,84 @@ variable "default_egress_priority" {
 
 # Deprecated variables
 variable "this_module_name" {
-  description = "(Deprecated) It has been deprecated from 1.6.0, and use `name` instead."
+  description = "(Deprecated) It has been deprecated from 2.0.0, and use `name` instead."
   default     = ""
 }
 variable "vpc_name" {
-  description = "(Deprecated) It has been deprecated from 1.6.0."
+  description = "(Deprecated) It has been deprecated from 2.0.0."
   default     = ""
 }
 
 variable "vpc_cidr" {
-  description = "(Deprecated) It has been deprecated from 1.6.0."
+  description = "(Deprecated) It has been deprecated from 2.0.0."
   default     = ""
 }
 
 variable "group_id" {
-  description = "(Deprecated) It has been deprecated from 1.6.0, and use `existing_group_id` instead."
+  description = "(Deprecated) It has been deprecated from 2.0.0, and use `existing_group_id` instead."
   default     = ""
 }
 
 variable "group_name" {
-  description = "(Deprecated) It has been deprecated from 1.6.0 and use 'name' instead."
+  description = "(Deprecated) It has been deprecated from 2.0.0 and use 'name' instead."
   default     = ""
 }
 
 variable "group_description" {
-  description = "(Deprecated) It has been deprecated from 1.6.0 and use 'name' instead."
+  description = "(Deprecated) It has been deprecated from 2.0.0 and use 'name' instead."
   default     = ""
 }
 
 variable "protocol" {
-  description = "(Deprecated) It has been deprecated from 1.6.0, and use `protocol_for_ingress_with_ports` and `protocol_for_egress_with_ports` instead."
+  description = "(Deprecated) It has been deprecated from 2.0.0, and use `protocol_for_ingress_with_ports` and `protocol_for_egress_with_ports` instead."
   type        = string
   default     = "tcp"
 }
 
 variable "priority" {
-  description = "(Deprecated) It has been deprecated from 1.6.0, and use `default_ingress_priority` and `default_egress_priority` instead."
+  description = "(Deprecated) It has been deprecated from 2.0.0, and use `default_ingress_priority` and `default_egress_priority` instead."
+  type        = number
+  default     = 1
+}
+variable "ingress_with_cidr_block" {
+  description = "(Deprecated) It has been deprecated from 2.1.0 and `ingress_with_cidr_blocks` instead. List of ingress rules to create where `cidr_block` is used. Each item's `cidr_block` can not be empty.  If some one item want to use `cidr_blocks`, the first one of `cidr_blocks` will be used."
+  type        = list(map(string))
+  default     = []
+}
+variable "ingress_with_ports" {
+  description = "(Deprecated) It has been deprecated from 2.1.0 and `ingress_ports` instead. The port list to use on all ingress ports rules. `from` and `to` have the same port. Example: [80, 443] means 80/80 and 443/443."
+  type        = list(number)
+  default     = []
+}
+variable "protocol_for_ingress_with_ports" {
+  description = "(Deprecated) It has been deprecated from 2.1.0 and `ingress_with_cidr_blocks_and_ports` instead. The default protocol where `ingress_with_ports` is used"
+  type        = string
+  default     = "tcp"
+}
+variable "priority_for_ingress_with_ports" {
+  description = "(Deprecated) It has been deprecated from 2.1.0 and `ingress_with_cidr_blocks_and_ports` instead. A priority is used when setting `ingress_with_ports`. Default to `default_ingress_priority`."
+  type        = number
+  default     = 1
+}
+variable "egress_with_cidr_block" {
+  description = "(Deprecated) It has been deprecated from 2.1.0 and `egress_with_cidr_blocks` instead. List of egress rules to create where `cidr_block` is used. Each item's `cidr_block` can not be empty. If some one item want to use `cidr_blocks`, the first one of `cidr_blocks` will be used."
+  type        = list(map(string))
+  default     = []
+}
+
+variable "egress_with_ports" {
+  description = "(Deprecated) It has been deprecated from 2.1.0 and `egress_ports` instead. The port list to use on all egress ports rules. `from` and `to` have the same port. Example: [80, 443] means 80/80 and 443/443."
+  type        = list(number)
+  default     = []
+}
+variable "protocol_for_egress_with_ports" {
+  description = "(Deprecated) It has been deprecated from 2.1.0 and `egress_with_cidr_blocks_and_ports` instead. A protocol where `egress_with_ports` is used."
+  type        = string
+  default     = "tcp"
+}
+
+variable "priority_for_egress_with_ports" {
+  description = "(Deprecated) It has been deprecated from 2.1.0 and `egress_with_cidr_blocks_and_ports` instead. A priority is used when setting `egress_with_ports`. Default to `default_egress_priority`."
   type        = number
   default     = 1
 }
--- modules/consul/main.tf
+++ modules/consul/main.tf
@@ -18,8 +18,8 @@ module "sg" {
   # Rules by names - open for default CIDR
   ingress_rules = sort(compact(distinct(concat(var.auto_ingress_rules, var.ingress_rules, [""]))))
 
-  # Open to IPv4 cidr blocks
-  ingress_with_cidr_block = var.ingress_with_cidr_block
+  # A priority is used when setting `ingress_rules`.
+  priority_for_ingress_rules = var.priority_for_ingress_rules
 
   # Open to IPv4 cidr blocks with a cidr block list
   ingress_with_cidr_blocks = var.ingress_with_cidr_blocks
@@ -27,17 +27,14 @@ module "sg" {
   # Open for security group id
   ingress_with_source_security_group_id = var.ingress_with_source_security_group_id
 
-  # The ingress port list
-  ingress_with_ports = var.ingress_with_ports
+  # Open to IPv4 cidr blocks and ports with a cidr block port list
+  ingress_with_cidr_blocks_and_ports = var.ingress_with_cidr_blocks_and_ports
 
-  # A protocol is used when setting `ingress_with_ports`
-  protocol_for_ingress_with_ports = var.protocol_for_ingress_with_ports
+  # The ingress port list
+  ingress_ports = var.ingress_with_ports
 
   # The IPv4 CIDR ranges list to use on ingress cidrs rules.
   ingress_cidr_blocks = var.ingress_cidr_blocks
-
-  # A priority is used when setting `ingress_rules`.
-  priority_for_ingress_rules = var.priority_for_ingress_rules
 
   # A default ingress priority.
   default_ingress_priority = var.default_ingress_priority
@@ -48,8 +45,8 @@ module "sg" {
   # Rules by names - open for default CIDR
   egress_rules = sort(compact(distinct(concat(var.auto_egress_rules, var.egress_rules, [""]))))
 
-  # Open to IPv4 cidr blocks
-  egress_with_cidr_block = var.egress_with_cidr_block
+  # A priority is used when setting `egress_rules`.
+  priority_for_egress_rules = var.priority_for_egress_rules
 
   # Open to IPv4 cidr blocks with a cidr block list
   egress_with_cidr_blocks = var.egress_with_cidr_blocks
@@ -57,17 +54,14 @@ module "sg" {
   # Open for security group id
   egress_with_source_security_group_id = var.egress_with_source_security_group_id
 
-  # The egress port list
-  egress_with_ports = var.egress_with_ports
+  # Open to IPv4 cidr blocks and ports with a cidr block port list
+  egress_with_cidr_blocks_and_ports = var.egress_with_cidr_blocks_and_ports
 
-  # A protocol is used when setting `egress_with_ports`
-  protocol_for_egress_with_ports = var.protocol_for_egress_with_ports
+  # The ingress port list
+  egress_ports = var.egress_with_ports
 
   # The IPv4 CIDR ranges list to use on egress cidrs rules.
   egress_cidr_blocks = var.egress_cidr_blocks
-
-  # A priority is used when setting `egress_rules`.
-  priority_for_egress_rules = var.priority_for_egress_rules
 
   # A default egress priority.
   default_egress_priority = var.default_egress_priority
